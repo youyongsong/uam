1e018821154e8d21f64cc9f58f17b58d79a79c3a
youyongsong <youyongsong@gmail.com>
2018-01-04 19:09:22 +0800

    feature: add aliases for common used subcommands. close #86

--- uam/interfaces/cli/__init__.py
+++ uam/interfaces/cli/__init__.py
@@ -1,8 +1,8 @@
 import click
 
-from .tap import tap
-from .app import app
-from .system import system
+from . import tap
+from . import app
+from . import system
 
 
 @click.group()
@@ -10,6 +10,55 @@ def uam():
     pass
 
 
-uam.add_command(tap)
-uam.add_command(app)
-uam.add_command(system)
+@click.command()
+@click.argument("app_name")
+@click.option("--pinned", default="")
+@click.pass_context
+def install(ctx, app_name, pinned):
+    ctx.forward(app.install)
+
+
+@click.command()
+@click.argument("app_name")
+@click.option("--pinned", default="")
+@click.pass_context
+def uninstall(ctx, app_name, pinned):
+    ctx.forward(app.uninstall)
+
+
+@click.command()
+@click.argument("app_name")
+@click.option("--pinned", default="")
+@click.pass_context
+def shell(ctx, app_name, pinned):
+    ctx.forward(app.exec_app)
+
+
+@click.command()
+@click.pass_context
+def ls(ctx):
+    ctx.forward(app.list_apps)
+
+
+@click.command()
+@click.argument("app_name")
+@click.pass_context
+def upgrade(ctx, app_name):
+    ctx.forward(app.upgrade_app)
+
+
+@click.command()
+@click.pass_context
+def init(ctx):
+    ctx.forward(system.init)
+
+
+uam.add_command(install)
+uam.add_command(uninstall)
+uam.add_command(shell)
+uam.add_command(ls)
+uam.add_command(upgrade)
+uam.add_command(init)
+uam.add_command(tap.tap)
+uam.add_command(app.app)
+uam.add_command(system.system)
